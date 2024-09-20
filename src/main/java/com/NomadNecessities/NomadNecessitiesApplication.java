d0081b978609b578983bc5ef365e03dfa3212358
Shubham <shubhamsharmadob@gmail.com>
2024-09-21 00:36:11 +0530

    Initial Commit

---
+++ src/main/java/com/NomadNecessities/NomadNecessitiesApplication.java
@@ -0,0 +1,13 @@
+package com.NomadNecessities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadNecessitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadNecessitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NomadNecessities/NomadNecessitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NomadNecessities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadNecessitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
